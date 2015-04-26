5d40e78f2e807a4fe13fbb346d2d9412b050d46e
wlwr <admin@wlwr.net>
2015-04-26 23:47:26 +0800

    set config

---
+++ config/weixin.js
@@ -0,0 +1,15 @@
+module.exports = {
+	api : {
+		getList : 'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=%s&page=1&t=%d' 
+	},
+
+	listCachePolicy : {
+		lifeTime : 43200,
+		cacheId : 'rss:list:%s'
+	},
+
+	contentCachePolicy : {
+		lifeTime: 864000,
+		cacheId: 'rss:content:%s'
+	}
+} 
