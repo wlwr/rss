9bde18d84d47e76026ee1ee8e8e4e0d71ce49469
wlwr <admin@wlwr.net>
2015-05-01 19:13:59 +0800

    cookie-pool

--- config/weixin.js
+++ config/weixin.js
@@ -3,7 +3,8 @@ module.exports = {
 	cacheEnable: true,
 
 	api: {
-		getList: 'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=%s&page=1&t=%d'
+		getList: 'http://weixin.sogou.com/gzhjs?cb=sogou.weixin.gzhcb&openid=%s&page=1&t=%d',
+		getCookie: 'http://weixin.sogou.com/weixin?query=%s'
 	},
 
 	listCachePolicy: {
@@ -14,5 +15,12 @@ module.exports = {
 	contentCachePolicy: {
 		lifeTime: 864000,
 		cacheId: 'rss:content:%s'
+	},
+
+	cookieNum : 10,
+
+	cookieCachePolicy: {
+		lifeTime: 86400,
+		cacheId: 'rss:cookies'
 	}
 }
